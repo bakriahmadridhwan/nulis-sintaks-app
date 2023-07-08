b28a1c6790317443193e15d74c6b2512305b8b95
bakriahmadridhwan <bahridhwan@gmail.com>
2023-07-09 01:26:36 +0700

    initial project - start setup

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="custom h-screen w-screen bg-sky-500">
+    <h1 class="text-3xl font-bold underline">Hello World !!!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
